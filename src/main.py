440b0d50fcf7641c8d99eb5f67f7c8b65de69e9f
Nicholas Mosier <nh.mosier@gmail.com>
2020-11-14 11:10:17 -0800

    works with mmap/munmap too

--- src/main.py
+++ src/main.py
@@ -108,7 +108,7 @@ class Variable(Value):
         return Variable(llvm_decl, llvm_decl.name)
 
     def new_symbol(self):
-        self.symbol = Symbol(self.symbol.symbol_name() + '\'', self.type.pysmt_type)
+        self.symbol = Symbol(self.symbol.symbol_name() + '^', self.type.pysmt_type)
         return self.symbol
     
 class Immediate(Value):
@@ -372,7 +372,7 @@ class Block(Value):
             if len(symbols) != 0:
                 assert len(symbols) == 1
                 symbol = symbols[0]
-                newf = substitute(formula, {symbol: Symbol(symbol.symbol_name() + '\'',
+                newf = substitute(formula, {symbol: Symbol(symbol.symbol_name() + '^',
                                                            get_type(symbol))})
                 self.successors[successor] = newf
         
@@ -392,6 +392,15 @@ class Block(Value):
         elif re.match(r'\s*br i1', last_inst_str):
             # conditional branch
             assert len(operand_strs) == 3
+            if operand_strs[0] == 'i1 true':
+                return {str2blk[operand_strs[2]]: TRUE(),
+                        str2blk[operand_strs[1]]: FALSE(),
+                        }
+            elif operand_strs[0] == 'i1 false':
+                return {str2blk[operand_strs[2]]: FALSE(),
+                        str2blk[operand_strs[1]]: TRUE(),
+                        }
+                
             var = str2var[operand_strs[0]]
             assert var.type.pysmt_type == BOOL
             # NOTE: Had to swap these for some strange reason.
@@ -486,7 +495,7 @@ def path_get_calls(path: list[Block], *args) -> list[Instruction]:
     
 class ExecutionEngine:
     def __init__(self, fn: Function, preds: list, assumptions: list, open_fn, close_fn,
-                 bmc: int = 10):
+                 bmc: int = 20):
         self.fn = fn
         self.preds = preds
         self.assumptions = assumptions
@@ -575,11 +584,11 @@ class ExecutionEngine:
                 for assumption in self.assumptions:
                     solver.add_assertion(assumption(path_blks, assignments, None))
 
-                correctness_formulas = dict(map(lambda pred: (pred(path_blks, assignments, None),
-                                                              self.preds[pred]),
-                                                self.preds))
-
-                retv = self.check_correctness_formulas(solver, correctness_formulas)
+                correctness_msgs = list(map(lambda pred: pred.msg, self.preds))
+                correctness_formulas = list(map(
+                    lambda pred: pred.pred(path_blks, assignments, pred.state), self.preds))
+                retv = self.check_correctness_formulas(solver,
+                                                       zip(correctness_formulas, correctness_msgs))
 
                 solver.pop()
 
@@ -590,13 +599,15 @@ class ExecutionEngine:
 
     def check_correctness_formulas(self, solver, formulas) -> bool:
         retv = True
-        for formula in formulas:
+        for (formula, msg) in formulas:
             solver.push()
             solver.add_assertion(formula)
             if solver.solve():
                 model = solver.get_model()
                 values = model.get_values(map(lambda arg: arg.symbol, self.fn.arguments))
-                print('INCORRECT: {}: {}'.format(formulas[formula], values))
+                print('INCORRECT: {}: {}'.format(msg, values))
+                print('FORMULA: {}'.format(formula))
+                print('MODEL:\n{}'.format(model))
                 retv = False
             solver.pop()
             if not retv:
@@ -604,26 +615,32 @@ class ExecutionEngine:
         return retv
         
     def run(self):
-        # start_blkname = str(len(list(self.fn.arguments)))
-        # start_blk = self.fn.blkname_to_block(start_blkname)
         start_blk = self.fn.blocks[0]
         assignments = dict(map(lambda arg: (arg, arg.symbol), fn.arguments))
         self.run_rec(start_blk, list(), assignments, SymbolicStore(self.fn))
 
 class FunctionModel:
     # rv_valid: callback that returns pysmt.formula determining wether r.v. of function call valid
-    def __init__(self, name: str, valid):
+    def __init__(self, name: str, valid, invalid):
         self.name = name
         self.valid = valid
+        self.invalid = invalid
 
+class CorrectnessPredicate:
+    def __init__(self, pred, init_state, msg):
+        self.pred = pred
+        self.state = init_state
+        self.msg = msg
+        
 class TwoCallVerifier:
     def __init__(self, open_fn, close_fn):
-        preds = {self.double_close_pred: 'double close',
-                 self.opens_have_close_pred: 'open w/o close',
-                 self.closes_have_open_pred: 'close w/o open',
-                 self.closes_valid_arg_pred: 'invalid argument to close',
-        }
-        assumptions = [self.opens_distinct_assumption, self.opens_valid_rv_assumption]
+        preds = [CorrectnessPredicate(self.double_close_pred, None, 'double close'),
+                 CorrectnessPredicate(self.opens_have_close_pred, list(), 'open w/o close'),
+                 CorrectnessPredicate(self.closes_have_open_pred, None, 'close w/o open'),
+                 CorrectnessPredicate(self.closes_valid_arg_pred, None,
+                                      'invalid argument to close'),
+        ]
+        assumptions = [self.opens_distinct_assumption] # , self.opens_valid_rv_assumption]
         self.open_fn = open_fn
         self.close_fn = close_fn
         self.eng = ExecutionEngine(fn, preds, assumptions, open_fn, close_fn)
@@ -633,7 +650,7 @@ class TwoCallVerifier:
 
     def get_calls(self, path: list[Block]) -> tuple:
         return path_get_calls(path, self.open_fn.name, self.close_fn.name)
-        
+
     def double_close_pred(self, path: list[Block], assignments: dict, state) -> pysmt.formula:
         (opens, closes) = self.get_calls(path)
         return Not(AllDifferent(*map(lambda c: c.operands[0].formula(assignments), closes)))
@@ -644,13 +661,19 @@ class TwoCallVerifier:
         
         (opens, closes) = self.get_calls(path)
 
+        # if valid(open), then find close
         def open_has_close(open: Instruction) -> pysmt.formula:
             return Or(*map(lambda close: EqualsOrIff(open.defined_variable.symbol,
                                                 close.operands[0].formula(assignments)), closes))
-        return Not(And(*map(open_has_close, opens)))
+
+        opens_have_close = map(open_has_close, opens)
+        opens_valid = map(lambda open: self.open_fn.valid(open, assignments), opens)
+        valid_opens_have_close = map(lambda t: Implies(*t), zip(opens_valid, opens_have_close))
+        return Not(And(*valid_opens_have_close))
 
     def closes_have_open_pred(self, path, assignments, state):
         (opens, closes) = self.get_calls(path)
+        print('opens {} closes {}'.format(len(opens), len(closes)))
         def close_has_open(close: Instruction) -> pysmt.formula:
             return Or(*map(lambda open: EqualsOrIff(open.defined_variable.symbol,
                                                close.operands[0].formula(assignments)), opens))
@@ -683,58 +706,62 @@ ll_path = args.file[0]
 module = Module.parse_file(ll_path)
 
 
-def double_close_pred(path: list[Block], assignments: dict, state) -> pysmt.formula:
-    (mallocs, frees) = path_get_calls(path, 'malloc', 'free')
-    free_uniqueness_formula = Not(AllDifferent(*map(lambda f: f.operands[0].formula(assignments),
-                                                frees)))
-    return free_uniqueness_formula
-
-def open_has_close_pred(path: list[Block], assignments: dict, state) -> pysmt.formula:
-    # permit path if it hasn't reached a return instruction
-    if not path[-1].returns:
-        return FALSE();
-
-    # find mallocs and frees
-    (mallocs, frees) = path_get_calls(path, 'malloc', 'free')
-    if len(mallocs) != len(frees):
-        print('calls to malloc() not equal to calls to free()')
-        return TRUE()
-    if len(mallocs) == 0:
-        return FALSE()
-
-    # ensure 1:1 correspondence to mallocs and frees
-    def malloc_equals_exactly_one_free(malloc: Instruction) -> pysmt.formula:
-        return ExactlyOne(*map(lambda free: EqualsOrIff(malloc.defined_variable.symbol,
-                                                   free.operands[0].formula(assignments)), frees))
-
-    pigeonhole_formula = Not(And(*map(malloc_equals_exactly_one_free, mallocs)))
-    return pigeonhole_formula
-
-
-def free_unallocated_ptr_pred(path: list[Block], assignments: dict, state) -> pysmt.formula:
-    (mallocs, frees) = path_get_calls(path, 'malloc', 'free')
-    if len(mallocs) != 0 or len(frees) == 0:
-        return FALSE()
-    return TRUE()
-    # TODO: Doesn't work for frees to values that haven't been previously malloc'ed.
-        
-    
 for fn in module.function_definitions:
     for blk in fn.blocks:
         continue
 
-    def pred(path: list[Block]) -> bool:
-        return True
-
+    def malloc_invalid_rv(inst, assignments) -> pysmt.formula:
+        return Equals(inst.defined_variable.symbol, BVZero(inst.defined_variable.type.bitwidth))
+    
     def malloc_valid_rv(inst, assignments) -> pysmt.formula:
         return NotEquals(inst.defined_variable.symbol, BVZero(inst.defined_variable.type.bitwidth))
 
-    def free_valid_arg(inst, assignments):
-        return NotEquals(inst.operands[0].formula(assignments),
-                         BVZero(inst.operands[0].value.type.bitwidth))
+    def open_valid_rv(inst, assignments) -> pysmt.formula:
+        return BVSGE(inst.defined_variable.symbol, BVZero(inst.defined_variable.type.bitwidth))
+
+    def open_invalid_rv(inst, assignments) -> pysmt.formula:
+        return BVSLT(inst.defined_variable.symbol, BVZero(inst.defined_variable.type.bitwidth))
+
+    def close_valid_arg(inst, assignments) -> pysmt.formula:
+        f = inst.operands[0].formula(assignments)
+        return BVSGE(f, BVZero(f.bv_width()))
+
+    def close_invalid_arg(inst, assignments) -> pysmt.formula:
+        f = inst.operands[0].formula(assignments)
+        return BVSLT(f, BVZero(f.bv_width()))
+
+    def mmap_valid_rv(inst, assignments) -> pysmt.formula:
+        return NotEquals(inst.defined_variable.symbol, SBV(-1, inst.defined_variable.type.bitwidth))
+
+    def mmap_invalid_rv(inst, assignments) -> pysmt.formula:
+        return Equals(inst.defined_variable.symbol, SBV(-1, inst.defined_variable.type.bitwidth))
+
+    def munmap_valid_arg(inst, assignments):
+        f = inst.operands[0].formula(assignments)
+        return NotEquals(f, SBV(-1, f.bv_width()))
+
+    def munmap_invalid_arg(inst, assignments):
+        f = inst.operands[0].formula(assignments)
+        return Equals(f, SBV(-1, f.bv_width()))
     
-    open_fn = FunctionModel('malloc', malloc_valid_rv)
-    close_fn = FunctionModel('free', free_valid_arg)
+    malloc_fn = FunctionModel('malloc', malloc_valid_rv, malloc_invalid_rv)
+    free_fn = FunctionModel('free', lambda a, b: TRUE(), lambda a, b: FALSE())
+
+    open_fn = FunctionModel('\x01_open', open_valid_rv, open_invalid_rv)
+    close_fn = FunctionModel('\x01_close', close_valid_arg, close_invalid_arg)
+
+    mmap_fn = FunctionModel('\x01_mmap', mmap_valid_rv, mmap_invalid_rv)
+    munmap_fn = FunctionModel('\x01_munmap', munmap_valid_arg, munmap_invalid_arg)
+
+    print('====== MALLOC/FREE =======')
+    verifier = TwoCallVerifier(malloc_fn, free_fn)
+    verifier.run()
+    
+    print('====== OPEN/CLOSE =======')
     verifier = TwoCallVerifier(open_fn, close_fn)
+    verifier.run()
+
+    print('====== MMAP/MUNMAP =======')
+    verifier = TwoCallVerifier(mmap_fn, munmap_fn)
     verifier.run()
     
